f1149011cf22d9eac9bb367e57de01443a1125ed
JunichiHashimoto <gitonly543@gmail.com>
2024-02-03 18:43:27 +0900

    Initial Commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by 橋本純一 on 2024/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 橋本純一 on 2024/02/03.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
